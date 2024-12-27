b1b31af9c264f64b3dbfe2f5326803f7af5a3fb5
Maximilian <scientificresearch309@gmail.com>
2024-12-27 14:10:52 +0100

    feat: implement getCountryData function to fetch and display multiple countries' data

---
+++ script_2.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+///////////////////////////////////////
+const getCountryData = (...country) => {
+  console.log(country); // ['portugal', 'germany', 'usa']
+
+  // Using forEach only because of the REST operator and created Array above:
+  country.forEach((country) => {
+    // There are multiple ways of doing AJAX calls in JS => we start with the most old school one which is XMLHttpRequest function:
+    const request = new XMLHttpRequest();
+    // Request is an object => Type of httpRequest to get the data is GET!
+    // https://github.com/public-apis/public-apis
+    // request.open("GET", "https://restcountries.com/v3.1/name/portugal");
+    request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+    request.send(); // => This send our GET request to the above URL!
+
+    // console.log(request.responseText); We will have here nothing
+
+    // The responseText will be accessible after the request has loaded
+    request.addEventListener("load", function () {
+      // console.log(request.responseText); // OR using this keyword points to the request
+      console.log(this.responseText); // this keyword refers to request object directly!
+
+      // this.responseText is in JSON format which is a big string of text and what we need is JS actual object! Therfore, we have to use JSON.parse() to convert the JSON string text to JS obejcts!
+
+      const [data] = JSON.parse(this.responseText);
+      console.log(data); // Now, we have an array containing an object full of info about Germany, therefore, we can destructure this object => after destructuring, we get only the object inside the array and no longer array is there!
+
+      // WITHOUT DESTRUCTURING, WE HAVE TO WRITE AS FOLLOWING: => BUT DESTRUCTURING IS MUCH MORE BEAUTIFUL THAN USING INDEXING IN AN ARRAY:
+      const data2 = JSON.parse(this.responseText)[0];
+      console.log(data2);
+
+      const html = `<article class="country">
+    <img class="country__img" src=${data.flags.png} />
+    <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${Number(
+              data.population / 1000000
+            ).toFixed(2)}M people
+            </p>
+            <p class="country__row"><span>🗣️</span>${Object.values(
+              data.languages
+            )}</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
+              </div>
+              </article>`;
+
+      // form.insertAdjacentHTML("afterend", html);
+      // Here we have class countries as parent of our html here instead of form. We don't have form here!
+      countriesContainer.insertAdjacentHTML("beforeend", html);
+
+      // To meke it appear on the page, we have to change the opacity to 1:
+      countriesContainer.style.opacity = 1;
+    });
+  });
+};
+
+// It depends, the info for which Country arrives sooner, therefore, addEventListener will listen to that and the info are loaded completely for this country, then the cakll back function will be called and the info for this country will be shown!
+
+// That's why when i reload every time the page, the sequence for the flags of countries will change and the order of displaying the flah will change and is not fiexd! In this case, we can not control or set which flag comes first and which comes later! It depends the info of which country is arrived from the ONLINE-API sooner!
+getCountryData("portugal", "germany", "usa");
+
+// NOTE: OR USING REST OPERATOR TO PUT ALL THE COUNTRIES TOGETHRE AN AN ARRAY AND WE NO LONGER NEED TO CALL EVERY COUNTRY SEPARATELY!
+// getCountryData("germany");
+// getCountryData("usa");
