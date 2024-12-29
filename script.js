5fbbc7a71b4e9d288dd88550a7156c35a80b3143
Maximilian <scientificresearch309@gmail.com>
2024-12-30 00:54:41 +0100

    refactor: streamline whereAmI function by integrating geolocation and fetch logic

--- script.js
+++ script.js
@@ -27,50 +27,48 @@ const getPosition = () => {
 };
 
 // getPosition().then((pos) => console.log(pos)); // when we have a resolved Promise, it means we got the Position successfully and we return it to the name of the function => getPosition(). We call the function, it has the position and returns it to the then method() and display it to the console()!
-getPosition().then((pos) => {
-  // console.log(pos);
-  const { latitude, longitude } = pos.coords;
-  // LET'S US TO BRING IT TO THE NEXT LEVEL: In last Coding-Challenge, we give the lat and lng parameters manually as input parameters to the function. BUT here we have them and we got them acutally as our current location(position), therefore, I give my current location(lat and lng) as inputs to the fetch function after a destructuring! => JUST THIS CHANGE AND ALL THE REMANING CODE REMAINS INTACT!
 
-  // const whereAmI = function (lat, lng) {
-  const response = fetch(
-    `https://geocode.xyz/${latitude},${longitude}?geoit=json`
-  )
-    .then((response) => {
-      console.log(response);
-      if (!response.ok) {
-        throw new Error(`Problem with geocoding! ${response.status}`);
-      }
-      return response.json(); // this returns a resolved promise as data in next then method!
-    })
-    .then((data) => {
-      console.log(data);
-      // if (data.country === undefined) {
-      if (!data.country) {
-        throw new Error(`You can only make 3 requests per second!`);
-      }
-      console.log(`You are in ${data.city}, ${data.country}`);
-      return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
-    })
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error(`Country not found! ${res.status}`);
-      }
-      return res.json(); // this returns a resolved promise as data in next then method!
-    })
-    .then((data) => {
-      renderCountry(data[0]);
-    })
-    .catch((err) =>
-      console.error(
-        `ERROR: Something went Wrong ☠️☠️☠️ ${err.message} Try again later!`
+const whereAmI = function () {
+  getPosition().then((pos) => {
+    // console.log(pos);
+    const { latitude: lat, longitude: lng } = pos.coords;
+    // LET'S US TO BRING IT TO THE NEXT LEVEL: In last Coding-Challenge, we give the lat and lng parameters manually as input parameters to the function. BUT here we have them and we got them acutally as our current location(position), therefore, I give my current location(lat and lng) as inputs to the fetch function after a destructuring! => JUST THIS CHANGE AND ALL THE REMANING CODE REMAINS INTACT!
+    const response = fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+      .then((response) => {
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(`Problem with geocoding! ${response.status}`);
+        }
+        return response.json(); // this returns a resolved promise as data in next then method!
+      })
+      .then((data) => {
+        console.log(data);
+        // if (data.country === undefined) {
+        if (!data.country) {
+          throw new Error(`You can only make 3 requests per second!`);
+        }
+        console.log(`You are in ${data.city}, ${data.country}`);
+        return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
+      })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Country not found! ${res.status}`);
+        }
+        return res.json(); // this returns a resolved promise as data in next then method!
+      })
+      .then((data) => {
+        renderCountry(data[0]);
+      })
+      .catch((err) =>
+        console.error(
+          `ERROR: Something went Wrong ☠️☠️☠️ ${err.message} Try again later!`
+        )
       )
-    )
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
-});
-// };
+      .finally(() => {
+        countriesContainer.style.opacity = 1;
+      });
+  });
+};
 
 const renderCountry = (data, className = "") => {
   const html = `<article class="country ${className}">
@@ -97,3 +95,10 @@ const renderCountry = (data, className = "") => {
   // I added to finally and commented it out here, because anyway it has to be happen no matter of success for promise or a failed promise!
   // countriesContainer.style.opacity = 1;
 };
+
+// btn.addEventListener("click", () => {
+//   whereAmI();
+// });
+
+// OR, because our function => whereAmI(); get no input parameter, we can write the above function simpler as following:
+btn.addEventListener("click", whereAmI);
