4f1a47718221679da61c464a82280afc27383010
Maximilian <scientificresearch309@gmail.com>
2024-12-29 18:56:18 +0100

    feat: add lotteryPromise implementation to demonstrate promise creation

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 
 console.log("-------------BUILDING A SIMPLE PROMISE--------------------------");
 
+// This is like the fetch function which also creates a new Promise!
+const lotteryPromise = new Promise((resolve, reject) => {
+  if (Math.random() >= 0.5) {
+    resolve("YOU WIN 💰");
+  }
+  reject("You lost your money 💩");
+});
