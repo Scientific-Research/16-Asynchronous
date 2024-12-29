cb3742553de17a463d18d6e32189940e3fe00bff
Maximilian <scientificresearch309@gmail.com>
2024-12-29 23:40:35 +0100

    feat: enhance geolocation script with additional logging for asynchronous behavior

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 
 console.log("-----------------------Promisifying the GEOLOCATIONS API-------");
 
+// In console.log() we see first of all the "Getting Position" and then the coordination because of the ASYNCHRONOUS BEHAVIOUR(NON-BLOCKING BEHAVIOUR)
+// Get Current Location is playing off at the background to get the location(IN WEB APIs area in Brwoser), In the meantime, JS goes to the next statement and run that!
 navigator.geolocation.getCurrentPosition(
   (position) => console.log(position),
   (err) => console.log(err)
 );
+
+console.log("Getting Position");
