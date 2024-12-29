df3fcf25e300e232e5e193e8a2864e32ab0f2efb
Maximilian <scientificresearch309@gmail.com>
2024-12-29 19:09:10 +0100

    feat: enhance lotteryPromise to handle rejection and improve error logging

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ console.log("-------------BUILDING A SIMPLE PROMISE--------------------------");
 // This is like the fetch function which also creates a new Promise!
 const lotteryPromise = new Promise((resolve, reject) => {
   if (Math.random() >= 0.5) {
-    resolve("YOU WIN 💰");
+    resolve("YOU WIN 💰"); // will appear in then() method as a resolved Promise
+  } else {
+    reject("You lost your money 💩"); // will appear in catch() method as a rejected Promise
   }
-  reject("You lost your money 💩");
 });
 
 // A promise is a special kind of an object, that's why lotteryPromise variable would be an object too and we can call a then method on that:
-lotteryPromise.then((res) => console.log(res)).catch((err) => console.err(err));
+lotteryPromise
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
