d10a889e3443d98e824678fbdd967705e119693e
Maximilian <scientificresearch309@gmail.com>
2024-12-30 00:04:59 +0100

    refactor: simplify geolocation handling by updating promise structure and removing unnecessary wait function

--- script.js
+++ script.js
@@ -14,27 +14,15 @@ console.log("Getting Position");
 // This is very clearly a callback-based API and we have to pass in two different callbacks which are position and err.
 
 // This is a very good opportunity to promisify a callback-based API to a Promised-based API:
-const wait = (seconds) => {
-  // We don't need the reject here, because a Timer will never fail, Therefore, we don't need to write it here! Promise is like array method(map) => In map, there are three parameters, but most of the time, we need one or maximum 2 of them!
-
-  // Callback function here would be simply resolve and we don't need to pass in any value in resolve function like above, This is not mandatory for a resolve function => NO resolved values are needed! We just need to wait our resolve() function with a certain amount of time here!
-  // resolve function will be executed for example after 2 seconds!
-  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
-};
-
-wait(2)
-  .then(() => {
-    console.log("I waited for 2 seconds!");
-    return wait(1);
-  })
-  .then(() => console.log("I waited for 1 second!"));
-
-const geolocation = () => {
-  return new Promise((position, err) =>
-    navigator.geolocation.getCurrentPosition(position, err)
+const getPosition = () => {
+  return new Promise((resolve, reject) =>
+    navigator.geolocation.getCurrentPosition(
+      (resolve) => console.log(resolve),
+      (reject) => console.log(reject)
+    )
   );
 };
 
-geolocation().then(() => {
+getPosition().then(() => {
   console.log();
 });
