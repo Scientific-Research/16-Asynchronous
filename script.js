08773b3addf4a0d89df7e2e99b0b581d8c392167
Maximilian <scientificresearch309@gmail.com>
2024-12-30 23:59:57 +0100

    fix: correct variable reference in fetch call within whereAmI function

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ console.log("------------------Consuming Promises with ASYNC/AWAIT------");
 
 const whereAmI = async () => {
   // Here we can have one or more AWAIT!
-  await fetch(`https://restcountries.com/v3.1/name/${data.country}`);
+  // fetch function return a promise and we can wait of that result here!
+  await fetch(`https://restcountries.com/v3.1/name/${country}`);
 };
