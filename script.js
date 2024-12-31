fd2e00b3f98a18a2d1fc722c353c6e186089ed00
Maximilian <scientificresearch309@gmail.com>
2024-12-31 12:04:52 +0100

    refactor: improve fetch handling in whereAmI function and enhance documentation clarity

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const whereAmI = async (country) => {
   // Here we can have one or more AWAIT statements!
   // after writing await word, we need a promise, which can come from a fetch function => fetch function return a promise and we can await of that result here!
   // AWAIT, wait for the result of the fetch function => it stops the code execution at this point until the promise is fullfilled(untill the data has been fetched in this case!)
-  await fetch(`https://restcountries.com/v3.1/name/${country}`);
+  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
 
-  // QUESTION: IS AWAIT BLOCK OUR CODE IN THIS POINT: OF COURSE NOT! in a async function the function is running in the background!
+  // QUESTION: IS AWAIT BLOCK OUR CODE IN THIS POINT: OF COURSE NOT! Stopping the execution of an async function in this point is not a problem because the async function is running asynchrounsly in the background, therefore it is not blocking the main thread of execution! and that is special about the async await function which is look like a regualr sync function but in fact in background is running asynchronously!
+
+  // As soon as fetch function is resolved(the results of fetching are there), then the result of whole process including await is resolved too and we can store that in a variable like res which is response!
 };
