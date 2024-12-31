8192ff39908634fc114c4f14479e3ab86e85e45c
Maximilian <scientificresearch309@gmail.com>
2024-12-31 21:14:48 +0100

    feat: add try-catch block to whereAmI function for improved error handling

--- script.js
+++ script.js
@@ -44,37 +44,41 @@ const getPosition = () => {
 // NOTE: ASYNC and AWAIT is only about consuming the Promises - The way we build them is not influence anyway!
 // const whereAmI = async (country) => {
 const whereAmI = async () => {
-  // Geolocation:
-  const pos = await getPosition();
-  const { latitude: lat, longitude: lng } = pos.coords;
+  try {
+    // Geolocation:
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
 
-  // Reverse geocoding:
-  const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-  const dataGeo = await resGeo.json();
-  console.log(dataGeo);
+    // Reverse geocoding:
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    const dataGeo = await resGeo.json();
+    console.log(dataGeo);
 
-  // Here we can have one or more AWAIT statements!
-  // after writing await word, we need a promise, which can come from a fetch function => fetch function return a promise and we can await of that result here!
-  // AWAIT, wait for the result of the fetch function => it stops the code execution at this point until the promise is fullfilled(untill the data has been fetched in this case!)
+    // Here we can have one or more AWAIT statements!
+    // after writing await word, we need a promise, which can come from a fetch function => fetch function return a promise and we can await of that result here!
+    // AWAIT, wait for the result of the fetch function => it stops the code execution at this point until the promise is fullfilled(untill the data has been fetched in this case!)
 
-  // Country data:
-  const res = await fetch(
-    `https://restcountries.com/v3.1/name/${dataGeo.country}`
-  );
-  const data = await res.json(); // this returns a new promise and previously we have to continue with .then chain! BUT NOW; we have to AWAIT this and assign it to a new variable! JUST THAT!!!
-  console.log(data);
-  renderCountry(data[0]);
+    // Country data:
+    const res = await fetch(
+      `https://restcountries.com/v3.1/name/${dataGeo.country}`
+    );
+    const data = await res.json(); // this returns a new promise and previously we have to continue with .then chain! BUT NOW; we have to AWAIT this and assign it to a new variable! JUST THAT!!!
+    console.log(data);
+    renderCountry(data[0]);
 
-  // QUESTION: IS AWAIT BLOCK OUR CODE IN THIS POINT: OF COURSE NOT! Stopping the execution of an async function in this point is not a problem because the async function is running asynchrounsly in the background, therefore it is not blocking the main thread of execution! and that is special about the async await function which is look like a regualr sync function but in fact in background is running asynchronously!
+    // QUESTION: IS AWAIT BLOCK OUR CODE IN THIS POINT: OF COURSE NOT! Stopping the execution of an async function in this point is not a problem because the async function is running asynchrounsly in the background, therefore it is not blocking the main thread of execution! and that is special about the async await function which is look like a regualr sync function but in fact in background is running asynchronously!
 
-  // As soon as fetch function is resolved(the results of fetching are there), then the result of whole process including await is resolved too and we can store that in a variable like res which is response!
-  // console.log(res);
+    // As soon as fetch function is resolved(the results of fetching are there), then the result of whole process including await is resolved too and we can store that in a variable like res which is response!
+    // console.log(res);
 
-  // NOTE: Async && Await are in fact simply syntatic sugar over the .then method in Promises!
-  // ABOVE ASYNC AND AWAIT ARE EQUIVALENT OF BELOW FETCH AND .THEN:
-  // fetch(`https://restcountries.com/v3.1/name/${country}`).then((res) =>
-  //   console.log(res)
-  // );
+    // NOTE: Async && Await are in fact simply syntatic sugar over the .then method in Promises!
+    // ABOVE ASYNC AND AWAIT ARE EQUIVALENT OF BELOW FETCH AND .THEN:
+    // fetch(`https://restcountries.com/v3.1/name/${country}`).then((res) =>
+    //   console.log(res)
+    // );
+  } catch (err) {
+    console.error(`Something went wrong! ${err.message}`);
+  }
 };
 // whereAmI("portugal");
 whereAmI();
@@ -88,11 +92,11 @@ console.log("FIRST"); // this will be displayed first because whereAmI is an asy
 
 console.log("-------------------TRY-CATCH BLOCK----------------------------");
 
-try {
-  let y = 1;
-  const x = 2;
-  x = 3;
-} catch (err) {
-  console.log("You can not assign a value to a constant!");
-  // alert(err.message); // Assignment to constant variable!
-}
+// try {
+//   let y = 1;
+//   const x = 2;
+//   x = 3;
+// } catch (err) {
+//   console.log("You can not assign a value to a constant!");
+//   // alert(err.message); // Assignment to constant variable!
+// }
