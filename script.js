31d6ed12ec305bee7b8b3607a87d0c2c8110ff05
Maximilian <scientificresearch309@gmail.com>
2024-12-31 01:06:10 +0100

    refactor: enhance whereAmI function documentation and clarify async behavior

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 console.log("------------------Consuming Promises with ASYNC/AWAIT------");
 
-const whereAmI = async () => {
-  // Here we can have one or more AWAIT!
-  // fetch function return a promise and we can wait of that result here!
+// async => using async to this function, we make it a special function => a function which is keep running in the background while performing the code inside of it and when is done, it returns a promise!
+const whereAmI = async (country) => {
+  // Here we can have one or more AWAIT statements!
+  // after writing await word, we need a promise, which can come from a fetch function => fetch function return a promise and we can await of that result here!
+  // AWAIT, wait for the result of the fetch function => it stops the code execution at this point until the promise is fullfilled(untill the data has been fetched in this case!)
   await fetch(`https://restcountries.com/v3.1/name/${country}`);
+
+  // QUESTION: IS AWAIT BLOCK OUR CODE IN THIS POINT: OF COURSE NOT! in a async function the function is running in the background!
 };
