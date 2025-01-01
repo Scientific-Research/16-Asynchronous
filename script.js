82b5bd90b461c0699011cbec5c015cdffc4bfc0a
Maximilian <scientificresearch309@gmail.com>
2025-01-01 16:25:19 +0100

    feat: add parallel promise handling for fetching multiple countries in get3Countries function

--- script.js
+++ script.js
@@ -153,3 +153,34 @@ console.log("-------------------TRY-CATCH BLOCK----------------------------");
 // }
 
 console.log("-------------Returning values from ASYNC Functions------------");
+
+console.log("------------- RUNNING PROMISES IN PARALLEL--------------------");
+
+// Imagine that we want to get the data from 3 different countries at the same time and order of received data doesn't matter at all:
+// RUNNING PROMISES IN PARALLEL:
+
+const get3Countries = async (c1, c2, c3) => {
+  // In an async function, we need to wrap up our function in a try-catch block
+  // NOTE: NEVER WORK A ASYNC FUNCTION WITHOUT TRY-CATCH BLOCK:
+  try {
+    const getJSON = (url, errorMsg = "Something went wrong!") => {
+      // NOTE: I have to return the fetch method => In this case, it return Promise like other then methods and therefore, I can chain it to other then methods below!
+      return fetch(url).then((response) => {
+        console.log(response);
+        // If we give a correct country name, we will get response.ok:true and response.status: 200
+        // If we give an incorrect country name, we will get response.ok:false and response.status:404
+
+        // when the response.ok is false, the promise which is returns from then method is rejected and the current then method terminates immediately! This rejected promise will propagate all the way down to the catch() handler below!
+
+        // The err.message that we see in catch() handler gets exactly the below message => Counry not found! ${response.status} and display it on the page!
+        if (!response.ok) {
+          // throw is like return, it returns the error!
+          throw new Error(`${errorMsg} ${response.status}`);
+        }
+        return response.json();
+      });
+    };
+  } catch (err) {
+    console.log(err);
+  }
+};
