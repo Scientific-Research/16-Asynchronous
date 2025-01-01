b2489a4d352788d9d951aa2a6fe2a359e616a4d9
Maximilian <scientificresearch309@gmail.com>
2025-01-01 16:44:36 +0100

    fix: update console log to include example output for clarity

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ const get3Countries = async (c1, c2, c3) => {
     const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
     const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
 
-    console.log(data1.capital, data2.capital, data3.capital);
+    console.log(data1.capital, data2.capital, data3.capital); // ['Lisbon'] ['Ottawa'] ['Dodoma']
   } catch (err) {
     console.log(err);
   }
