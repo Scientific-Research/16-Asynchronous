55d350dd7010538f8c9c2e2fe71d0ba7246f47e9
Maximilian <scientificresearch309@gmail.com>
2025-01-01 16:38:46 +0100

    refactor: move getJSON function outside of get3Countries for better structure and reusability

--- script.js
+++ script.js
@@ -28,6 +28,24 @@ const renderCountry = (data, className = "") => {
   countriesContainer.style.opacity = 1;
 };
 
+const getJSON = (url, errorMsg = "Something went wrong!") => {
+  // NOTE: I have to return the fetch method => In this case, it return Promise like other then methods and therefore, I can chain it to other then methods below!
+  return fetch(url).then((response) => {
+    console.log(response);
+    // If we give a correct country name, we will get response.ok:true and response.status: 200
+    // If we give an incorrect country name, we will get response.ok:false and response.status:404
+
+    // when the response.ok is false, the promise which is returns from then method is rejected and the current then method terminates immediately! This rejected promise will propagate all the way down to the catch() handler below!
+
+    // The err.message that we see in catch() handler gets exactly the below message => Counry not found! ${response.status} and display it on the page!
+    if (!response.ok) {
+      // throw is like return, it returns the error!
+      throw new Error(`${errorMsg} ${response.status}`);
+    }
+    return response.json();
+  });
+};
+
 const renderError = (msg) => {
   // It does the same like insertAdjacentHtml but with text, that's why i added the msg to that(last time a html file added with insertAdjacentHtml)
   countriesContainer.insertAdjacentText("beforeend", msg);
@@ -162,25 +180,16 @@ console.log("------------- RUNNING PROMISES IN PARALLEL--------------------");
 const get3Countries = async (c1, c2, c3) => {
   // In an async function, we need to wrap up our function in a try-catch block
   // NOTE: NEVER WORK A ASYNC FUNCTION WITHOUT TRY-CATCH BLOCK:
+
   try {
-    const getJSON = (url, errorMsg = "Something went wrong!") => {
-      // NOTE: I have to return the fetch method => In this case, it return Promise like other then methods and therefore, I can chain it to other then methods below!
-      return fetch(url).then((response) => {
-        console.log(response);
-        // If we give a correct country name, we will get response.ok:true and response.status: 200
-        // If we give an incorrect country name, we will get response.ok:false and response.status:404
+    const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
 
-        // when the response.ok is false, the promise which is returns from then method is rejected and the current then method terminates immediately! This rejected promise will propagate all the way down to the catch() handler below!
-
-        // The err.message that we see in catch() handler gets exactly the below message => Counry not found! ${response.status} and display it on the page!
-        if (!response.ok) {
-          // throw is like return, it returns the error!
-          throw new Error(`${errorMsg} ${response.status}`);
-        }
-        return response.json();
-      });
-    };
+    console.log(data1.capital, data2.capital, data3.capital);
   } catch (err) {
     console.log(err);
   }
 };
+
+get3Countries("portugal", "canada", "tanzania");
